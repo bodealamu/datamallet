7bf3c40f51ba87b4ba0e2fd61e966f3215b4c7ea
olabodealamu <bodealamu007@gmail.com>
2021-12-05 14:34:26 -0600

    added column use

--- datamallet/visualization/utils.py
+++ datamallet/visualization/utils.py
@@ -28,3 +28,61 @@ def pie_sectors(df, maximum_number_sectors=3):
 
     return columns_list
 
+
+def __combine_object_categorical(df):
+    """
+    Combined columns of types categorical, object, and boolean into a list
+    :param df: pandas dataframe
+    :return:
+    """
+
+    combined = list()
+
+    if check_dataframe(df=df):
+        col_types = extract_col_types(df=df)
+
+        categorical = col_types['categorical']
+        object_cols = col_types['object']
+        boolean_cols = col_types['boolean']
+
+        combined.extend(categorical)
+        combined.extend(object_cols)
+        combined.extend(boolean_cols)
+
+    return combined
+
+
+def column_use(df, threshold=5):
+    """
+    This function helps in determining whether a column in a dataframe
+       should be used to color the data points in a chart or if it is
+        okay for simply naming the points
+    :param df: pandas dataframe
+    :param threshold: int, think of it as the number
+           of distinct colors in a chart(e.g scatterplot)
+    :return:
+    """
+    all_categorical_cols = __combine_object_categorical(df=df)
+
+    number_of_rows = df.shape[0]
+
+    column_use_dict = dict()
+    names_list = list()
+    hue_list = list()
+
+    for col in all_categorical_cols:
+        number_of_unique = len(get_unique(df=df, col_name=col))
+        ratio = number_of_rows / number_of_unique
+
+        if ratio < threshold:
+            names_list.append(col)
+
+        else:
+            hue_list.append(col)
+
+    column_use_dict['name'] = names_list
+    column_use_dict['hue'] = hue_list
+
+    return column_use_dict
+
+
