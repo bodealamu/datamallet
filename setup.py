9f2ddc88fb67af6a9c7255f7f528ebd825ae2b97
olabodealamu <bodealamu007@gmail.com>
2021-12-10 18:32:40 -0600

    bump version number to 0.1.2 after refactor

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="datamallet",
-    version="0.1.1",
+    version="0.1.2",
     description="Helper tools for the data science workflow",
     long_description=long_description,
     long_description_content_type="text/markdown",
