bd734fd5cec42229f15648a51b92d4068b59280d
olabodealamu <bodealamu007@gmail.com>
2021-12-12 21:14:14 -0600

    edited column adder and added tests

--- tests/test_tabular/test_features.py
+++ tests/test_tabular/test_features.py
@@ -7,11 +7,16 @@ df = pd.DataFrame({'A':[1,2,3,4,5],
                    'D':['male','male','male','female','female'],
                    'E':[True,True,False,True,True]})
 
+df2 = pd.DataFrame({'C':['dog','cat', 'sheep','dog','cat'],
+                   'D':['male','male','male','female','female']})
+
 
 def test_column_adder():
     column_adder = ColumnAdder(column_list=['A','B'],new_column_name='Z')
     added_df = column_adder.transform(X=df)
+    non_added = ColumnAdder(column_list=['C','D'],new_column_name='W').transform(X=df2)
 
     assert 'Z' in added_df.columns
     assert added_df['A'].sum() + added_df['B'].sum() == added_df['Z'].sum()
+    assert 'W' not in non_added.columns # ColumnAdder wont work for text columns
 
