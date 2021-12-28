4976c4d912bd25b6824bce7bd395a3027e0b280e
olabodealamu <bodealamu007@gmail.com>
2021-12-28 10:41:57 -0600

    tests completed for tabular/utils

--- tests/test_visualization/test_utils.py
+++ tests/test_visualization/test_utils.py
@@ -39,6 +39,26 @@ def test_columns_with_distinct_values():
     assert 'F' not in column_list, "the number of unique items in column F exceeds the maximum"
 
 
+def test_column_use():
+    col_types = extract_col_types(df=df)
+    column_use_dict = column_use(df, col_types, threshold=5)
+    column_use_dict2 = column_use(df, col_types, threshold=2)
+    assert isinstance(column_use_dict, dict)
+    assert len(column_use_dict['name']) != 0
+    assert len(column_use_dict['hue']) == 0
+    assert len(column_use_dict2['name']) != 0
+    assert len(column_use_dict2['hue']) != 0
+
+
+def test_create_pairs():
+    pairs = create_pairs(df=df2, numeric_cols=['A','B','C','D'])
+    pairs1 = create_pairs(df=df, numeric_cols=['A', 'B', 'C', 'D'])
+    pairs2 = create_pairs(df=df, numeric_cols=['A', 'B'])
+    assert len(pairs) != 0
+    assert len(pairs1) == 0,"only columns A and B in df are numeric"
+    assert len(pairs2) != 0, "only numeric cols are supplied"
+
+
 def test_hierachial_path():
     treemap_paths = hierarchical_path(df=df, limit=3, col_types=extract_col_types(df=df))
     treemap_paths3 = hierarchical_path(df=df3, limit=3, col_types=extract_col_types(df=df3))
@@ -52,21 +72,9 @@ def test_hierachial_path():
     assert isinstance(treemap_paths4, list)
 
 
-def test_create_pairs():
-    pairs = create_pairs(df=df2, numeric_cols=['A','B','C','D'])
-    pairs1 = create_pairs(df=df, numeric_cols=['A', 'B', 'C', 'D'])
-    assert len(pairs) != 0
-    assert len(pairs1) == 0
 
 
-def test_column_use():
-    col_types = extract_col_types(df=df)
-    column_use_dict = column_use(df, col_types, threshold=5)
-    column_use_dict2 = column_use(df, col_types, threshold=2)
-    assert isinstance(column_use_dict, dict)
-    assert len(column_use_dict['name']) != 0
-    assert len(column_use_dict['hue']) == 0
-    assert len(column_use_dict2['name']) != 0
-    assert len(column_use_dict2['hue']) != 0
+
+
 
 
