b58ddded433320f61ea877946318a0cb6e5074b6
olabodealamu <bodealamu007@gmail.com>
2022-01-07 18:53:24 -0600

    edited columnadder

--- datamallet/tabular/feature.py
+++ datamallet/tabular/feature.py
@@ -14,11 +14,7 @@ class ColumnAdder(BaseEstimator, TransformerMixin):
         :param new_column_name:str, name of new column created from adding the columns in column list together
 
         Usage
-        df = pd.DataFrame({'A':[1,2,3,4,5],
-                   'B':[2,4,6,8,10],
-                   'C':['dog','cat', 'sheep','dog','cat'],
-                   'D':['male','male','male','female','female'],
-                   'E':[True,True,False,True,True]})
+        >>> df = pd.DataFrame({'A':[1,2,3,4,5],'B':[2,4,6,8,10],'C':['dog','cat', 'sheep','dog','cat'],'D':['male','male','male','female','female'],'E':[True,True,False,True,True]})
 
         >>> column_adder = ColumnAdder(column_list=['A','B'],new_column_name='Z')
         >>> added_df = column_adder.transform(X=df)
